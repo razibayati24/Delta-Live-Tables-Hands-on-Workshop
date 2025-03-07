98b39564230203e5b249e6950eb571b0b0c61ad0
razibayati24 <razibayati24>
2025-03-07 19:39:30 +0000

    schema eval tests

--- notebooks/03. Process Change Data Capture (CDC) data In DLT Pipeline.py
+++ notebooks/03. Process Change Data Capture (CDC) data In DLT Pipeline.py
@@ -5,65 +5,66 @@
 
 # MAGIC %md
 # MAGIC **When creating the pipeline add the following configuration to point to the data source:**
-# MAGIC 
+# MAGIC
 # MAGIC `source` : `/tmp/delta-stream-dltworkshop/{your_user_name}/cdc_raw`
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ## Importance of Change Data Capture (CDC)
-# MAGIC 
+# MAGIC
 # MAGIC Change Data Capture (CDC) is the process that captures the changes in records made to a data storage like Database, Data Warehouse, etc. These changes usually refer to operations like data deletion, addition and updating.
-# MAGIC 
+# MAGIC
 # MAGIC A straightforward way of Data Replication is to take a Database Dump that will export a Database and import it to a LakeHouse/DataWarehouse/Lake, but this is not a scalable approach. 
-# MAGIC 
+# MAGIC
 # MAGIC Change Data Capture, only capture the changes made to the Database and apply those changes to the target Database. CDC reduces the overhead, and supports real-time analytics. It enables incremental loading while eliminates the need for bulk load updating.
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ### CDC Approaches 
-# MAGIC 
+# MAGIC
 # MAGIC **1- Develop in-house CDC process:** 
-# MAGIC 
+# MAGIC
 # MAGIC ***Complex Task:*** CDC Data Replication is not a one-time easy solution. Due to the differences between Database Providers, Varying Record Formats, and the inconvenience of accessing Log Records, CDC is challenging.
-# MAGIC 
+# MAGIC
 # MAGIC ***Regular Maintainance:*** Writing a CDC process script is only the first step. You need to maintain a customized solution that can map to aformentioned changes regularly. This needs a lot of time and resources.
-# MAGIC 
+# MAGIC
 # MAGIC ***Overburdening:*** Developers in companies already face the burden of public queries. Additional work for building customizes CDC solution will affect existing revenue-generating projects.
-# MAGIC 
+# MAGIC
 # MAGIC **2- Using CDC tools** such as Debezium, Hevo Data, IBM Infosphere, Qlik Replicate, Talend, Oracle GoldenGate, StreamSets.
-# MAGIC 
+# MAGIC
 # MAGIC In this demo repo we are using CDC data coming from a CDC tool. 
 # MAGIC Since a CDC tool is reading database logs:
 # MAGIC We are no longer dependant on developers updating a certain column 
-# MAGIC 
+# MAGIC
 # MAGIC — A CDC tool like Debezium takes care of capturing every changed row. It records the history of data changes in Kafka logs, from where your application consumes them. 
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ### Implement CDC In DLT Pipeline: Change Data Capture (Python)
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC <img src="https://raw.githubusercontent.com/morganmazouchi/Delta-Live-Tables/main/Images/dlt%20end%20to%20end%20flow.png">
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ###### Resource [Change data capture with Delta Live Tables](https://docs.databricks.com/data-engineering/delta-live-tables/delta-live-tables-cdc.html)
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ### Retail CDC Data Generator
 
 # COMMAND ----------
 
 # Full username, e.g. "<first>.<last>@databricks.com"
-username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+# username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+username = 'razi.bayati@databricks.com'
 
 # Short form of username, suitable for use as part of a topic name.
 user = username.split("@")[0].replace(".","_")
@@ -123,41 +124,41 @@ spark.read.json(folder+"/customers").display()
 # MAGIC %md
 # MAGIC ## How to synchronize your SQL Database with your Lakehouse? 
 # MAGIC CDC flow with a CDC tool, autoloader and DLT pipeline:
-# MAGIC 
+# MAGIC
 # MAGIC - A CDC tool reads database logs, produces json messages that includes the changes, and streams the records with changes description to Kafka
 # MAGIC - Kafka streams the messages  which holds INSERT, UPDATE and DELETE operations, and stores them in cloud object storage (S3 folder, ADLS, etc).
 # MAGIC - Using Autoloader we incrementally load the messages from cloud object storage, and stores them in Bronze table as it stores the raw messages 
 # MAGIC - Next we can perform APPLY CHANGES INTO on the cleaned Bronze layer table to propagate the most updated data downstream to the Silver Table
-# MAGIC 
+# MAGIC
 # MAGIC Here is the flow we'll implement, consuming CDC data from an external database. Note that the incoming could be any format, including message queue such as Kafka.
 # MAGIC <img src="https://raw.githubusercontent.com/morganmazouchi/Delta-Live-Tables/main/Images/cdc_flow_new.png" alt='Make all your data ready for BI and ML'/>
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ###How does CDC tools like Debezium output looks like?
-# MAGIC 
+# MAGIC
 # MAGIC A json message describing the changed data has interesting fields similar to the list below: 
-# MAGIC 
+# MAGIC
 # MAGIC - operation: an operation code (DELETE, APPEND, UPDATE, CREATE)
 # MAGIC - operation_date: the date and timestamp for the record came for each operation action
-# MAGIC 
+# MAGIC
 # MAGIC Some other fields that you may see in Debezium output (not included in this demo):
 # MAGIC - before: the row before the change
 # MAGIC - after: the row after the change
-# MAGIC 
+# MAGIC
 # MAGIC To learn more about the expected fields check out [this reference](https://debezium.io/documentation/reference/stable/connectors/postgresql.html#postgresql-update-events)
 
 # COMMAND ----------
 
 # MAGIC %md-sandbox
 # MAGIC ### Incremental data loading using Auto Loader (cloud_files)
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC Working with external system can be challenging due to schema update. The external database can have schema update, adding or modifying columns, and our system must be robust against these changes.
 # MAGIC Databricks Autoloader (`cloudFiles`) handles schema inference and evolution out of the box.
-# MAGIC 
+# MAGIC
 # MAGIC Autoloader allow us to efficiently ingest millions of files from a cloud storage, and support efficient schema inference and evolution at scale. In this notebook we leverage Autoloader to handle streaming (and batch) data.
 # MAGIC <div style="float:down">
 # MAGIC   <img width="1000px" src="https://user-images.githubusercontent.com/85911675/178177635-249deecd-9261-4586-8032-565c534b5e9f.png"/>
@@ -168,13 +169,13 @@ spark.read.json(folder+"/customers").display()
 
 # MAGIC %md
 # MAGIC ## DLT Python Syntax 
-# MAGIC 
+# MAGIC
 # MAGIC It's necessary to import the `dlt` Python module to use the associated methods. Here, we also import `pyspark.sql.functions`.
-# MAGIC 
+# MAGIC
 # MAGIC DLT tables, views, and their associated settings are configured using [decorators](https://www.python.org/dev/peps/pep-0318/#current-syntax).
-# MAGIC 
+# MAGIC
 # MAGIC If you're unfamiliar with Python decorators, just note that they are functions or classes preceded with the `@` sign that interact with the next function present in a Python script.
-# MAGIC 
+# MAGIC
 # MAGIC The `@dlt.table` decorator is the basic method for turning a Python function into a Delta Live Table.
 
 # COMMAND ----------
@@ -187,7 +188,7 @@ from pyspark.sql.types import *
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ## Set up Configurations
 
 # COMMAND ----------
@@ -243,11 +244,11 @@ def customer_bronze_clean_v():
 
 # MAGIC %md-sandbox
 # MAGIC ## Materializing the silver table
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://raw.githubusercontent.com/morganmazouchi/Delta-Live-Tables/main/Images/cdc_silver_layer.png" alt='Make all your data ready for BI and ML' style='float: right' width='1000'/>
-# MAGIC 
+# MAGIC
 # MAGIC The silver `customer_silver` table will contains the most up to date view. It'll be a replicate of the original table.
-# MAGIC 
+# MAGIC
 # MAGIC To propagate the `Apply Changes Into` operations downstream to the `Silver` layer, we must explicitly enable the feature in pipeline by adding and enabling the applyChanges configuration to the DLT pipeline settings.
 
 # COMMAND ----------
@@ -283,5 +284,5 @@ dlt.apply_changes(
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC Next step, create DLT pipeline, add a path to this notebook and **add configuration with enabling applychanges to true**. 
